e9faccbefa558bdd9ce26e31b33052e5eb3e601d
Miguel Angel Feo Merchan <mfeomerchan@gmail.com>
2025-02-11 14:19:52 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Nombres from "./componentes/Nombres"; // Asegúrate de que la ruta es correcta
+
+function App() {
+  return (
+    <div>
+      <Nombres />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/componentes/Nombres.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+
+const ListaNombres = () => {
+    const [nombre, setNombre] = useState("");
+    const [nombres, setNombres] = useState([]);
+
+    const guardar = () => {
+        setNombres([...nombres, nombre]);
+        setNombre("");
+    };
+
+    return (
+        <div>
+            <p>Ingresa un nombre: </p>
+            <input type="text" value={nombre} onChange={(e) => setNombre(e.target.value)} />
+            <button onClick={guardar}>Enviar</button>
+            <ul>
+                {nombres.map((elemento, index) => (
+                    <li key={index}>{elemento}</li>
+                ))}
+            </ul>
+        </div>
+    );
+};
+
+export default ListaNombres;
