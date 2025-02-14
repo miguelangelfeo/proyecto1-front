780b9a1b4360eba50b2e1093484cfc2f27b9f005
Miguel Angel Feo Merchan <mfeomerchan@gmail.com>
2025-02-14 18:50:13 -0500

    arregla ruta

--- src/componentes/Nombres.js
+++ src/componentes/Nombres.js
@@ -11,22 +11,24 @@ const ListaNombres = () => {
     }, []);
     const cargarNombres = async () => {
         try {
-            const response = await axios.get("http://104.248.106.174/backend/api/nombres-bd");
+            const response = await axios.get("http://104.248.106.174/api/nombres-bd");
             setNombres(response.data);
         } catch (error) {
             console.error("Error al obtener los nombres", error);
         }
     };
+
     const guardar = async () => {
         if (nombre.trim() === "") return;
         try {
-            await axios.post("http://104.248.106.174/backend/api/nombre", { nombre });
-            setNombre("");  // Limpiar el input
-            cargarNombres(); // Recargar los nombres después de guardar
+            await axios.post("http://104.248.106.174/api/nombre", { nombre });
+            setNombre("");
+            cargarNombres();
         } catch (error) {
             console.error("Error al guardar el nombre", error);
         }
     };
+
 
     return (
         <div>
